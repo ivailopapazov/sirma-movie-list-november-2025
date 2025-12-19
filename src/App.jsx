aadb957333bd61835869e011c25ce60ab6af9296
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-12-19 18:39:37 +0200

    Add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Footer from "./components/footer/Footer"
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
+import './App.css'
+import Login from "./components/login/Login"
 
 function App() {
     return (
@@ -8,7 +10,8 @@ function App() {
             <Header />
 
             <main className="mx-auto container-max px-4 py-10">
-                <Home />
+                {/* <Home /> */}
+                <Login />
             </main>
 
             <Footer />
