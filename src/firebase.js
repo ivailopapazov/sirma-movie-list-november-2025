586fa62712f013b97f0af19523368c35d710b714
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-12-19 18:30:53 +0200

    Add firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVswYM3iqFfv2VPQSor8xvye8zMpgYxkQ",
+  authDomain: "sirma-movie-list-november-2025.firebaseapp.com",
+  projectId: "sirma-movie-list-november-2025",
+  storageBucket: "sirma-movie-list-november-2025.firebasestorage.app",
+  messagingSenderId: "969362446478",
+  appId: "1:969362446478:web:8e594cdf5d3de1fdcd91df"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
